19128e617c67e28e9381eb34405841170fa4b8b7
Bot <1999eb@gmail.com>
2021-09-30 18:42:32 +0200

    Design ready

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,6 +15,9 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.FirstName;
+            label2.Text = Resource1.LastName;
+            button1.Text = Resource1.Add;
         }
     }
 }
