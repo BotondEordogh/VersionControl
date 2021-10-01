5cb62726909a62f05da200fe9b78642a0b6f7faa
Bot <1999eb@gmail.com>
2021-10-01 11:22:29 +0200

    Add write to file function

--- UserMaintenance/Entities/User.cs
+++ UserMaintenance/Entities/User.cs
@@ -9,7 +9,7 @@ namespace UserMaintenance.Entities
     class User
     {
         public Guid ID { get; set; } = Guid.NewGuid();
-        public string FullName {get; set;   }
+        public string FullName { get; set; }
 
     }
 }
