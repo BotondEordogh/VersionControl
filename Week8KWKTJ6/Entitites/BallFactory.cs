f07608ab76343191c314abd1d56188748801ecba
Bot <1999eb@gmail.com>
2021-11-11 18:44:57 +0100

    Create classes

---
+++ Week8KWKTJ6/Entitites/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week8KWKTJ6.Entitites
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
